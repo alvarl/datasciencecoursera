8a418484c119760053b950e70412c3f297e52fa7
Alvar Lumberg <alvar.lumberg@gmail.com>
2016-05-15 21:23:24 +0300

    removes unneeded group_by

--- EDA-courseproject-2/plot5.R
+++ EDA-courseproject-2/plot5.R
@@ -6,14 +6,12 @@ motorVehicleSCCs <- SCC[SCC$Data.Category == "Onroad", ]
 motorVehicleSCCs <- as.character(motorVehicleSCCs[, "SCC"])
 
 baltimoreMVRecords <- pm25all[pm25all$fips == "24510" & pm25all$SCC %in% motorVehicleSCCs,]
-baltimoreMVRecords <- group_by(baltimoreMVRecords, SCC)
 
 sumEmissions <- aggregate(data = baltimoreMVRecords, Emissions ~ year, sum)
 sumEmissions$year <- as.character(sumEmissions$year)
 
 plot <- ggplot(sumEmissions, aes(x = year, y = Emissions, group = 1)) 
 plot + geom_line() + ggtitle("Total PM2.5 emissions in Baltimore \nfrom motor vehicle sources 1998-2008")
-
 ggsave("plot5.png", width = 6, height = 3)
 dev.off()
 
