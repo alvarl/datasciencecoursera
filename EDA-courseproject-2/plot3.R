1de237591cc5a33603c847ee04b3d667d172ef92
Alvar Lumberg <alvar.lumberg@gmail.com>
2016-05-15 14:42:37 +0300

    Chart of total emissions by type 1998-2008 in Baltimore

---
+++ EDA-courseproject-2/plot3.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+pm25all <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+baltimoreData <- pm25all[pm25all$fips == "24510",]
+totalByYear <- aggregate(baltimoreData$Emissions ~ baltimoreData$year + baltimoreData$type, baltimoreData, sum)
+names(totalByYear) <- c("year", "type", "emissions")
+plot <- ggplot(totalByYear) + geom_line(aes(x= year, y = emissions, group = type, colour = type))
+plot + ggtitle("Change in PM2.5 emissions by type in Baltimore 1998-2008")
+ggsave("plot3.png", width = 6, height = 3)
+dev.off()
+
