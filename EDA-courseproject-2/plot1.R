df566eb5016e2a3a67d62cc7230e68fbefd3215f
Alvar Lumberg <alvar.lumberg@gmail.com>
2016-05-15 09:54:38 +0300

    saves plot1 to PNG

--- EDA-courseproject-2/plot1.R
+++ EDA-courseproject-2/plot1.R
@@ -2,6 +2,8 @@ pm25all <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 totalByYear <- aggregate(pm25all$Emissions ~ pm25all$year, pm25all, sum)
 names(totalByYear) <- c("year", "emissions")
+png("plot1.png", width = 480, height = 480, units = "px")
 plot(totalByYear, xaxt = "n")
 axis(side = 1, at = totalByYear$year,labels = T)
+dev.off()
 
